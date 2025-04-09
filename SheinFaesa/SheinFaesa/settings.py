b5e8a4a418de904fb899169f7e4a0718705b2e0c
Matheus Moura <matheusmouradasilva2018@gmail.com>
2025-04-08 22:34:55 -0300

    Retirando arquivos da tentativa 1

--- SheinFaesa/SheinFaesa/settings.py
+++ SheinFaesa/SheinFaesa/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Produtos',
 ]
 
 MIDDLEWARE = [
