2f152527471eb430cca1fc07d9911d90aac5cd8f
Matheus Moura <matheusmouradasilva2018@gmail.com>
2025-04-12 00:10:32 -0300

    atualizando arquivos

--- SheinFaesa/wsgi.py
+++ SheinFaesa/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for SheinFaesa project.
+WSGI config for loja project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SheinFaesa.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'loja.settings')
 
 application = get_wsgi_application()
+
